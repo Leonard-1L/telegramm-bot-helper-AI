6593b8a76749f349dbf87827718ead21574236c3
Leonard-1L <lprofi52@gmail.com>
2024-03-16 22:22:24 +0300

    yep

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ DB_NAME = 'users.db'
 DB_TABLE_USERS_NAME = 'users'
 MAX_REQUESTS_TOKENS = 128
 BOT_TOKEN = "6565406578:AAHJXXP9C3YuGGrZavTnkhlJTq4hQl1V3KE"
+# TODO - это все примеры, пропиши свое если у тебя отличаются данные.
